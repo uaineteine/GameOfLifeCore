9b3cd1c2146627e27026da6bcf0841f94445125b
Daniel Robert <Daniel.Robert.S-S@hotmail.com>
2020-09-20 10:37:04 +1000

    user input added in

--- GameOfLife/Program.cs
+++ GameOfLife/Program.cs
@@ -10,7 +10,16 @@ namespace GameOfLife
 
             //load it
             gamelife ca = new gamelife(20, 20);
-            ca.Simulate(10);
+
+            //get user input
+            int noSteps = 0;
+            Console.WriteLine("Give number of steps to simulate:");
+            Console.WriteLine();
+            string res = Console.ReadLine();
+            noSteps = Convert.ToInt32(res);
+
+            //simulate
+            ca.Simulate(noSteps);
 
             Console.WriteLine("Press enter to exit");
             Console.ReadLine();
